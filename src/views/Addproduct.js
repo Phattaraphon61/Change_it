30e33a527bbeb2f80760be55cb1ce4d983793e22
patnikorn tai-ngoen <patnikorn.ta.61@ubu.ac.th>
2020-10-15 08:50:12 +0700

    แก้หน้าเพิ่มสินค้า

--- src/views/Addproduct.js
+++ src/views/Addproduct.js
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
-import React, { Component, useEffect ,useState} from "react";
-=======
 import React, { Component, useEffect, useState } from "react";
->>>>>>> 504d881041652bc479a2b3914f579c39ee3d4e7c
 import ReactDOM from 'react-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
-<<<<<<< HEAD
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import Autocomplete from '@material-ui/lab/Autocomplete';
@@ -17,15 +12,6 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-=======
-import Button from "@material-ui/core/Button";
-import {
- 
-  Typography,
-
-} from "@material-ui/core";
-import axios from "axios"
->>>>>>> 504d881041652bc479a2b3914f579c39ee3d4e7c
 import '../shards-dashboard/styles/Addproduct.css'
 // import $ from "jquery"
 
@@ -128,7 +114,6 @@ const useStyles = makeStyles((theme) => ({
 
 
 export default function Addproduct() {
-<<<<<<< HEAD
   const [provincess, setProvincess] = useState([
     { title: 'กรุงเทพมหานคร' },
     { title: 'สมุทรปราการ' },
@@ -202,7 +187,7 @@ export default function Addproduct() {
     { title: 'สงขลา' },
     { title: 'สตูล' },
     { title: 'ตรัง' },
-    { title: 'พัทลุง'   },
+    { title: 'พัทลุง' },
     { title: 'ปัตตานี' },
     { title: 'ยะลา' },
     { title: 'นราธิวาส' },
@@ -213,14 +198,6 @@ export default function Addproduct() {
   const [currency, setCurrency] = React.useState('plzselect');
   const [age, setAge] = React.useState('');
   const bull = <span className={classes.bullet}>•</span>;
-=======
-  const [fileInputState, setFileInputState] = useState("");
-  const classes = useStyles();
-  const [value, setValue] = React.useState('');
-  const [currency, setCurrency] = React.useState('EUR');
-  const [selectedFile, setSelectedFile] = useState("");
-  const [previewSource, setPreviewSource] = useState("");
->>>>>>> 504d881041652bc479a2b3914f579c39ee3d4e7c
 
   const handleChange = (event) => {
     setValue(event.target.value);
@@ -247,97 +224,29 @@ export default function Addproduct() {
     let formData = new FormData();
     formData.append("selectedFile", selectedFile);
 
-    axios.post("http://localhost:8080/image", formData).then(result => {});
-  
-};
+    axios.post("http://localhost:8080/image", formData).then(result => { });
+
+  };
   return (
     <div className={classes.root}>
-<<<<<<< HEAD
-=======
-      <Grid
-        container
-        justify="center"
-        alignItems="center"
-      >
->>>>>>> 504d881041652bc479a2b3914f579c39ee3d4e7c
 
       <br></br>
       <center><h2><b>สินค้าของฉัน</b></h2></center>
       <br></br>
 
       <Grid
-          container
-          direction="row"
-          justify="space-evenly"
-          alignItems="stretch"
+        container
+        direction="row"
+        justify="space-evenly"
+        alignItems="stretch"
       >
         <Grid item xs={5}>
           <Paper className={classes.paper}>
-<<<<<<< HEAD
-          <form className={classes.root} noValidate autoComplete="off">
-      <div>
-
-      <center><h4><b>คำอธิบายสินค้า</b></h4></center>
-
-      <TextField
-          id="outlined-multiline-flexible"
-          label="ชื่อสินค้า (จำเป็น)"
-          multiline
-          rowsMax={4}
-          value={value}
-          onChange={handleChange}
-          variant="outlined"
-        />
-        </div>
-    </form>
-    <br></br>
-    <form className={classes.root} noValidate autoComplete="off">
-      <div>
-      <TextField
-          id="outlined-multiline-static"
-          label="รายละเอียดสินค้า (จำเป็น)"
-          multiline
-          rows={4}
-          variant="outlined"
-        />
-      </div>
-    </form>
-    <br></br>
-    <form className={classes.root} noValidate autoComplete="off">
-      <div>
-    <TextField
-          id="standard-textarea"
-          label="เพิ่มแฮชแท็ก (#Apple)"
-          placeholder=" "
-          multiline
-        />
-         </div>
-    </form>
-
-    <br></br>
-    
-    <form className={classes.root} noValidate autoComplete="off">
-      <div>
-      <TextField
-          id="outlined-select-currency"
-          select
-          label="หมวดหมู่ (จำเป็น)"
-          value={currency}
-          onChange={handleChangetag}
-          helperText="กรุณาเลือกหมวดหมู่เพื่อการค้นหาที่สะดวกมากยิ่งขึ้น"
-          variant="outlined"
-        >
-          {currencies.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-      </div>
-    </form>
-=======
             <form className={classes.root} noValidate autoComplete="off">
               <div>
+
+                <center><h4><b>คำอธิบายสินค้า</b></h4></center>
+
                 <TextField
                   id="outlined-multiline-flexible"
                   label="ชื่อสินค้า (จำเป็น)"
@@ -348,40 +257,6 @@ export default function Addproduct() {
                   variant="outlined"
                 />
               </div>
-              <Typography align="center">
-                {previewSource !== "" ? (
-                  <img
-                    src={previewSource}
-                    width="170"
-                  ></img>
-                ) : (
-                    <div></div>
-                  )}
-              </Typography>
-              <Button
-                variant="outlined"
-                color="primary"
-                component="label"
-                className={
-                  classes.chooseImage
-                }
-                onChange={e => {
-                  handleFileInputChange(
-                    e
-                  );
-                }}
-                value={fileInputState}
-              >
-                เลือกรูปภาพ
-             <input
-                  type="file"
-                  style={{
-                    display: "none"
-                  }}
-                />
-              </Button>
-
-              <Button onClick={uploadPicture}> sende</Button>
             </form>
             <br></br>
             <form className={classes.root} noValidate autoComplete="off">
@@ -401,7 +276,7 @@ export default function Addproduct() {
                 <TextField
                   id="standard-textarea"
                   label="เพิ่มแฮชแท็ก (#Apple)"
-                  placeholder="Placeholder"
+                  placeholder=" "
                   multiline
                 />
               </div>
@@ -414,10 +289,10 @@ export default function Addproduct() {
                 <TextField
                   id="outlined-select-currency"
                   select
-                  label="Select"
+                  label="หมวดหมู่ (จำเป็น)"
                   value={currency}
                   onChange={handleChangetag}
-                  helperText="Please select your currency"
+                  helperText="กรุณาเลือกหมวดหมู่เพื่อการค้นหาที่สะดวกมากยิ่งขึ้น"
                   variant="outlined"
                 >
                   {currencies.map((option) => (
@@ -428,58 +303,56 @@ export default function Addproduct() {
                 </TextField>
               </div>
             </form>
->>>>>>> 504d881041652bc479a2b3914f579c39ee3d4e7c
 
-    <br></br>
+            <br></br>
 
-    <FormControl variant="outlined" className={classes.formControl}>
-    ที่อยู่ของสิ่งของ (จำเป็น)
+            <FormControl variant="outlined" className={classes.formControl}>
+              ที่อยู่ของสิ่งของ (จำเป็น)
         <InputLabel id="demo-simple-select-outlined-label"></InputLabel>
-        <br></br>
-        <Autocomplete
-          fullWidth name="province"
-          id="demo-simple-select-outlined"
-          options={provincess}
-          onChange={option => {console.log(option.target.innerText)}}
-          getOptionLabel={(option) => option.title}
-          renderInput={(params) => <TextField {...params} label="จังหวัด" variant="outlined" />}
-          label="จังหวัด"
-        >
-        </Autocomplete>
-      </FormControl>
+              <br></br>
+              <Autocomplete
+                fullWidth name="province"
+                id="demo-simple-select-outlined"
+                options={provincess}
+                onChange={option => { console.log(option.target.innerText) }}
+                getOptionLabel={(option) => option.title}
+                renderInput={(params) => <TextField {...params} label="จังหวัด" variant="outlined" />}
+                label="จังหวัด"
+              >
+              </Autocomplete>
+            </FormControl>
           </Paper>
         </Grid>
-<<<<<<< HEAD
         <Grid item xs={5}>
           <Paper className={classes.paper}>
-          <center><h4><b>อัพโหลดรูปภาพ</b></h4></center>
-          <Card className={classes.root} variant="outlined">
-            <CardContent>
-              Upload Your Product Image
+            <center><h4><b>อัพโหลดรูปภาพ</b></h4></center>
+            <Card className={classes.root} variant="outlined">
+              <CardContent>
+                Upload Your Product Image
             </CardContent>
-        
-          <CardActions>
-          <input
-            accept="image/*"
-            className={classes.input}
-            id="contained-button-file"
-            multiple
-            type="file"
-          />
-        
-          <label htmlFor="contained-button-file">
-                <Button variant="contained" color="primary" component="span">
-                อัพโหลด
+
+              <CardActions>
+                <input
+                  accept="image/*"
+                  className={classes.input}
+                  id="contained-button-file"
+                  multiple
+                  type="file"
+                />
+
+                <label htmlFor="contained-button-file">
+                  <Button variant="contained" color="primary" component="span">
+                    อัพโหลด
               </Button>
-          </label>
-          </CardActions>
-        </Card>
+                </label>
+              </CardActions>
+            </Card>
             <br></br>
-        <Button variant="contained" color="secondary" href="#contained-buttons">
-                ส่งข้อมูลสินค้า
+            <Button variant="contained" color="secondary" href="#contained-buttons">
+              ส่งข้อมูลสินค้า
               </Button>
-              <p>________________</p>
-              <p>Attention ! : กรุณาเช็คความถูกต้องก่อนกดส่งข้อมูลสินค้า</p>
+            <p>________________</p>
+            <p>Attention ! : กรุณาเช็คความถูกต้องก่อนกดส่งข้อมูลสินค้า</p>
           </Paper>
         </Grid>
 
@@ -487,23 +360,17 @@ export default function Addproduct() {
           <br></br>
           <center><h4><b>WARNING !!!</b></h4></center>
           <Paper className={classes.paper}>
-          <p><b>________________ ข้อควรระวังเกี่ยวกับการแลกเปลี่ยน ________________</b></p>
-          <p>เพื่อปกป้องสิทธิของคุณเราขขอแนะนำให้คุณมีการเจรจาซื้อขายที่นี่และตรวจสอบให้แน่ใจว่าให้ข้อมูลเกี่ยวกับการแลกเปลี่ยนที่เพียงพอ 
-หากคุณใช้โปรแกรมสนทนาอื่นนอกเหนือจากที่นี่ เราจะไม่สามารถบันทึกกระบวนการแลกเปลี่ยนทั้งหมดของคุณได้อย่างสมบรูณ์
+            <p><b>________________ ข้อควรระวังเกี่ยวกับการแลกเปลี่ยน ________________</b></p>
+            <p>เพื่อปกป้องสิทธิของคุณเราขขอแนะนำให้คุณมีการเจรจาซื้อขายที่นี่และตรวจสอบให้แน่ใจว่าให้ข้อมูลเกี่ยวกับการแลกเปลี่ยนที่เพียงพอ
+            หากคุณใช้โปรแกรมสนทนาอื่นนอกเหนือจากที่นี่ เราจะไม่สามารถบันทึกกระบวนการแลกเปลี่ยนทั้งหมดของคุณได้อย่างสมบรูณ์
 จึงขอความร่วมมือมา ณ ที่นี้ </p>
           ___________________________________________________________________
           </Paper>
         </Grid>
-=======
->>>>>>> 504d881041652bc479a2b3914f579c39ee3d4e7c
 
       </Grid>
       <br></br>
     </div>
 
-<<<<<<< HEAD
   );
-=======
-
->>>>>>> 504d881041652bc479a2b3914f579c39ee3d4e7c
 }
